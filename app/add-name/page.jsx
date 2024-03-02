2a7e3776f0f4a763d95c75a46a75e01a7d0bda92
Radoslav Phan Van <phanvan.radoslav@gmail.com>
2024-03-02 10:11:15 +0100

    removed unecessary files and code

--- app/add-name/page.jsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-export default function AddName() {
-  return (
-    <>
-      <h1>add Name</h1>
-    </>
-  );
-}
--- app/page.jsx
+++ app/page.jsx
@@ -1,26 +1,10 @@
-import Image from "next/image";
-import styles from "./page.module.css";
-import prisma from "@/lib/prisma";
-import Name from "@/app/components/name";
 import Link from "next/link";
-
-async function getNames() {
-  const response = await fetch("http://localhost:3000/api");
-  const names = await response.json();
-  const namesToRender = await names.map((name) => {
-    <div key={name.id}>{name.name}</div>;
-  });
-
-  return namesToRender;
-}
 
 export default async function Home() {
   const names = await getNames();
   return (
     <main>
-      <Link href={"/add-name"}>Add Name</Link>
-      <h1>Feed</h1>
-      {names}
+      <Link href={"/get-names"}>Get names api</Link>
     </main>
   );
 }
