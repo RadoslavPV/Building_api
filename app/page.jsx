74a377486126ce1f1b9d0d03de775207d1aac2a4
Radoslav Phan Van <100495320+RadoslavPV@users.noreply.github.com>
2024-03-04 14:23:36 +0000

    deleted usseles line

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,6 @@
 import Link from "next/link";
 
 export default async function Home() {
-  const names = await getNames();
   return (
     <main>
       <Link href={"/get-names"}>Get names api</Link>
