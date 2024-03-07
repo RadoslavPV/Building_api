a21792da21e7ecd5aafb1cf9bb950b1067fa2ded
Radoslav Phan Van <phanvan.radoslav@gmail.com>
2024-03-07 17:45:42 +0100

    added functional dynamic page //getting some errors in console

---
+++ app/get-names/[id]/page.jsx
@@ -0,0 +1,23 @@
+"use client";
+
+export default function IdPage({ params }) {
+  async function getName(params) {
+    const response = await fetch("http://localhost:3000/api/");
+    const names = await response.json();
+
+    let nameToDisplay;
+
+    names.forEach((element) => {
+      if (element.id == params.id) {
+        nameToDisplay = element.name;
+      }
+    });
+    return nameToDisplay;
+  }
+
+  return (
+    <>
+      <h1>{getName(params)}</h1>
+    </>
+  );
+}
