0d6dc6801a57eb553c79cd1c6d3215f18aa4f660
Radoslav Phan Van <phanvan.radoslav@gmail.com>
2024-03-09 18:10:08 +0100

    fixed error with delete api

--- app/delete-names/DeleteAll.jsx
+++ app/delete-names/DeleteAll.jsx
@@ -2,7 +2,7 @@
 
 export default function DeleteAll() {
   async function DeleteAllNames() {
-    response = await fetch("http://localhost:3000/api", {
+    fetch("http://localhost:3000/api", {
       method: "DELETE",
     });
   }
