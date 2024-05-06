a13f6321df76a5c2c8522d54bab6f25a79c201da
Radoslav Phan Van <phanvan.radoslav@gmail.com>
2024-05-06 15:51:58 +0200

    added button

--- app/page.jsx
+++ app/page.jsx
@@ -3,7 +3,11 @@ import Link from "next/link";
 export default async function Home() {
   return (
     <main>
-      <Link href={"/get-names"}>Get names api</Link>
+      <Link href={"/get-names"}>
+        <>
+          <button>Get names api</button>
+        </>
+      </Link>
     </main>
   );
 }
